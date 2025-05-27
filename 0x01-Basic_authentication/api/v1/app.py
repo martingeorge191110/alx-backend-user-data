329e265cfb0b43f7726ab0f6ba4b8d3403c5f77e
Martin George <martingeorge191110@gmail.com>
2025-05-27 16:20:04 +0300

    Task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -15,7 +16,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-if getenv('AUTH_TYPE') == 'auth':
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+elif auth_type == 'auth':
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module for BasicAuth class"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class that inherits from Auth"""
+    pass 
