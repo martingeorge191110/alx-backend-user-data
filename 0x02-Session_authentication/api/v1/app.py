c87b531886e296d95ece305f77e5e5925c64b2ff
Martin George <martingeorge191110@gmail.com>
2025-05-29 16:15:09 +0300

    adding task 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ auth = None
 auth_type = getenv('AUTH_TYPE')
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    auth = SessionAuth()
 elif auth_type == 'auth':
     auth = Auth()
 
